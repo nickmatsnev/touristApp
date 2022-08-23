0f8cfae4ca76881d31df1afc4cfd5bc1d33f4c97
nickmatsnev <matsnev2001@yandex.ru>
2022-08-24 00:53:47 +0200

    removed it

--- tourism/src/main/java/com/example/tourism/api/dto/TripDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.tourism.api.dto;
-
-public class TripDTO {
-}
--- tourism/src/main/java/com/example/tourism/business/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.tourism.business.services;
-
-import com.example.tourism.data.entities.UserEntity;
-
-
-public interface UserService extends CrudService<UserEntity, Integer> {
-
-    UserEntity createUser(int user_id, String name, String surname, String email, String password, String wallet);
-}
